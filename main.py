9ad0f2c5ea065612f7745bd62fdd3eb82b4c0ce0
Aby Bigasin <tetragonal@Abys-MBP.home>
2020-08-06 21:40:42 -0700

    Main file to run experiment.

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import numpy as np
+import Bio
+import sys
+from Bio import SeqIO
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+from Consensus import consensus
+from blastgenes import blast
+from compiler import comp
+#main file to run
+if __name__=="__main__":
+	#finding consensus between all programs first
+	cons = consensus()
+	cons.readfile(sys.argv[1])
+	print('Finding significant genes...')
+	cons.find_sig(cons.output)
+	print('Finding consensus...')
+	cons.find_consensus()
+	print('Finding upregulated genes...')
+	cons.find_upreg()
+	#blasting genes against ncbi database
+	dat = blast()
+	print('\n...\n...\n')
+	print("Reading in config file...")
+	dat.readfile(sys.argv[1])
+	print("Finding sequences to blast...")
+	dat.find_sequences(dat.fastapath,cons.consensus)
+	print("Blasting against NCBI database...")
+	dat.execute_blast(dat.records,dat.output,dat.newlist)
+	compiler = comp()
+	compiler.compiler(dat.output)
